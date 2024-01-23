8d411b7f16bc40582f5a20896956e8019a3f6e7b
Ezee1234 <akriti09bhardwaj@gmail.com>
2024-01-23 22:52:17 +0530

    committed codefirst project

---
+++ CodeFirst_CRUD/Models/StudentDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CodeFirst_CRUD.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirst_CRUD.Models
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions options) : base(options) 
+        {
+            
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
+
+   
